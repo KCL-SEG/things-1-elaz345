d084471ad15305009bc8ffb7ef6561a024c8cb9a
Emily Lazenby <124580748+elaz345@users.noreply.github.com>
2023-12-06 21:04:41 +0000

    ex2

--- things/models.py
+++ things/models.py
@@ -6,3 +6,6 @@ class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+
+    def __str__(self):
+        return self.name
